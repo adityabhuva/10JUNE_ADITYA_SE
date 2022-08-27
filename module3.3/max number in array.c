d583bef4b5f940ace816be1ff12547ba67d16c0f
adityabhuva <109803959+adityabhuva@users.noreply.github.com>
2022-08-27 14:42:56 +0200

    Add files via upload

---
+++ module3.3/max number in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int no=10,i,a[no],largenumber;
+    printf("enter the %d numbers in array.\n",no);
+    for(i=0;i<no;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    largenumber = a[0];
+    for(i=1;i<no;i++)
+    {
+        if(largenumber<a[i])
+            
+            largenumber = a[i];
+    }
+    printf("\n largest number of array :%d\n",largenumber);
+    
+    
+}
